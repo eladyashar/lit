48adb3f9dce3b08a8738f13a9cc482beccea47b0
Elad Yashar <eladyashar1104@gmail.com>
2022-01-04 13:52:31 +0200

    add static pages route to server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors=require('cors');
+const path = require('path')
 require('dotenv').config(); 
 // const { customersRouter } = require("./routers/customersRouter");
 // const { suppliersRouter } = require("./routers/suppliersRouter");
@@ -11,7 +12,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/static', express.static(path.join(__dirname, '../client')))
 //app.use('/lit/user', customersRouter);
 
 // app.use('/lit/suppliers', suppliersRouter); 
